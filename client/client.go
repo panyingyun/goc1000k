e9397f77b9b6391e9e5ba05566711ea98b6f1025
panyy <panyy@wtvdev.com>
2017-10-15 12:45:20 +0800

    useful client

--- client/client.go
+++ client/client.go
@@ -4,13 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"runtime"
 	"time"
 )
 
 var quitSemaphore chan bool
 
 func main() {
-	tcpAddr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:9999")
+	tcpAddr, _ := net.ResolveTCPAddr("tcp", "121.42.35.23:10000")
 	for i := 0; i < 1000; i++ {
 		conn, _ := net.DialTCP("tcp", nil, tcpAddr)
 		go onMessageRecived(conn)
@@ -34,7 +35,7 @@ func onMessageRecived(conn *net.TCPConn) {
 			quitSemaphore <- true
 			break
 		}
-		fmt.Printf("Connect [%v]\n", conn.LocalAddr().String())
+		fmt.Printf("Connect [%v] [%v]\n", conn.LocalAddr().String(), runtime.NumGoroutine())
 		time.Sleep(60 * time.Second)
 		b := []byte(msg)
 		conn.Write(b)
--- mclient/client.go
+++ mclient/client.go
@@ -4,19 +4,77 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
 	"time"
+
+	"github.com/urfave/cli"
 )
 
-var quitSemaphore chan bool
+func run(c *cli.Context) error {
+	ipaddr := c.String("ipaddr")
+	port := c.Int("port")
+	servernumber := c.Int("number")
+	connectnum := c.Int("connetcnt")
+
+	for i := 0; i < servernumber; i++ {
+		server := fmt.Sprintf("%v:%v", ipaddr, port+i)
+		fmt.Printf("Server Addr: [%v]\n", server)
+		go connectToServer(server, connectnum)
+	}
+	//quit when receive end signal
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	fmt.Printf("signal received signal %v\n", <-sigChan)
+	fmt.Println("shutting down server")
+	return nil
+}
 
 func main() {
-	tcpAddr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:9999")
-	for i := 0; i < 1000; i++ {
+	app := cli.NewApp()
+	app.Name = "c1000k-client"
+	app.Usage = "c1000k-client"
+	app.Copyright = "panyingyun@gmail.com"
+	app.Version = "0.1.0"
+	app.Action = run
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "ipaddr,ip",
+			Usage:  "Set Server start ip address here",
+			Value:  "127.0.0.1",
+			EnvVar: "IPADDR",
+		},
+		cli.IntFlag{
+			Name:   "port,p",
+			Usage:  "Set Server start port here",
+			Value:  10000,
+			EnvVar: "PORT",
+		},
+		cli.IntFlag{
+			Name:   "number,n",
+			Usage:  "Set Number of Server here",
+			Value:  1,
+			EnvVar: "NUMBER",
+		},
+		cli.IntFlag{
+			Name:   "connetcnt,cn",
+			Usage:  "Set Connet Count of Server here",
+			Value:  1,
+			EnvVar: "CONNETCNT",
+		},
+	}
+	app.Run(os.Args)
+}
+
+func connectToServer(server string, connectnum int) {
+	tcpAddr, _ := net.ResolveTCPAddr("tcp", server)
+	for i := 0; i < connectnum; i++ {
 		conn, _ := net.DialTCP("tcp", nil, tcpAddr)
 		go onMessageRecived(conn)
-		time.Sleep(1000 * time.Microsecond)
+		time.Sleep(500 * time.Microsecond)
 	}
-	<-quitSemaphore
 }
 
 func onMessageRecived(conn *net.TCPConn) {
@@ -31,11 +89,10 @@ func onMessageRecived(conn *net.TCPConn) {
 		msg, err := reader.ReadString('\n')
 		if err != nil {
 			conn.Close()
-			quitSemaphore <- true
 			break
 		}
-		fmt.Printf("Connect [%v]\n", conn.LocalAddr().String())
-		time.Sleep(60 * time.Second)
+		fmt.Printf("Connect [%v] [%v]\n", conn.LocalAddr().String(), runtime.NumGoroutine())
+		time.Sleep(600 * time.Second)
 		b := []byte(msg)
 		conn.Write(b)
 	}
